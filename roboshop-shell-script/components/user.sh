8dbfbaadb4c56882121d5058a3417913242042a9
Priyatham32 <87533020+Priyatham32@users.noreply.github.com>
2022-06-18 18:19:22 +0530

    d21

--- roboshop-shell-script/components/user.sh
+++ roboshop-shell-script/components/user.sh
@@ -1,1 +1,15 @@
-echo install user component
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
+cd /home/roboshop
+rm -rf user
+unzip /tmp/user.zip
+mv user-main user
+cd /home/roboshop/user
+npm install
+sed -i -e 's/REDIS_ENDPOINT/172.31.92.6' -e 's/MONGO_ENDPOINT/172.31.87.219' /home/roboshop/user/systemd.service
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl start user
+systemctl enable user
